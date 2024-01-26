2fe263b91bad997e01dfe8d63a222b90516727fa
andy3400 <grayhat3400@gmail.com>
2024-01-26 14:09:43 +0900

    Feat: connecting tact

--- src/contracts/faucetJettonWallet.ts
+++ src/contracts/faucetJettonWallet.ts
@@ -1,23 +1,23 @@
-import { fromNano } from "ton";
-import {
-  Contract,
-  ContractProvider,
-  Sender,
-  Address,
-  Cell,
-  contractAddress,
-  beginCell,
-  toNano,
-} from "ton-core";
+// import { fromNano } from "ton";
+// import {
+//   Contract,
+//   ContractProvider,
+//   Sender,
+//   Address,
+//   Cell,
+//   contractAddress,
+//   beginCell,
+//   toNano,
+// } from "ton-core";
 
-export default class FaucetJettonWallet implements Contract {
-  async getBalance(provider: ContractProvider) {
-    const { stack } = await provider.get("get_wallet_data", []);
-    return fromNano(stack.readBigNumber());
-  }
+// export default class FaucetJettonWallet implements Contract {
+//   async getBalance(provider: ContractProvider) {
+//     const { stack } = await provider.get("get_wallet_data", []);
+//     return fromNano(stack.readBigNumber());
+//   }
 
-  constructor(
-    readonly address: Address,
-    readonly init?: { code: Cell; data: Cell }
-  ) {}
-}
+//   constructor(
+//     readonly address: Address,
+//     readonly init?: { code: Cell; data: Cell }
+//   ) {}
+// }
